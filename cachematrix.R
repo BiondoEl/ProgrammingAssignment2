fb334fca9af34772d75f31a5768cc4c121305b04
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#function to define matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #cached inverse
+  inv <- NULL
+  
+  #matrix get / set functions
+  get <- function() x
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #inverse of matrix get / set functions
+  getinv <- function() inv
+  setinv <- function(solve) inv <<- solve
+  
+  #return list of all functions
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+#function to retrieve inverse of matrix (cached if present)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+retrievematrixinv <- function(x, ...) {
+  inv <- x$getinv()
+  
+  #return cached inverse if present
+  if(!is.null(inv)) {
+    message("getting cached matrix")
+    return(inv)
+  }
+  
+  # define inverse of matrix
+  matrixdata <- x$getinv()
+  inv <- solve(matrixdata, ...)
+  
+  #cache inverse of matrix
+  x$setinv(inv)
+  
+  #return inverse of cached matrix
+  return(inv)
 }
+
+
